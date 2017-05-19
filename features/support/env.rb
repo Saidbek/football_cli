5135474d823ab6c800eb39fcbc11ebf3ed61da92
Dennis Walters <dwalters@engineyard.com>
2017-05-19 17:21:05 -0400

    Configured Aruba to use the Runner

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,14 @@
 require 'aruba/cucumber'
+require 'football_cli/runner'
 
 ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 LIB_DIR = File.join(File.expand_path(File.dirname(__FILE__)),'..','..','lib')
+
+Aruba.configure do |config|
+  config.home_directory = File.join(config.root_directory, config.working_directory)
+  config.command_launcher = :in_process
+  config.main_class = FootballCli::Runner
+end
 
 Before do
   # Using "announce" causes massive warnings on 1.9.2

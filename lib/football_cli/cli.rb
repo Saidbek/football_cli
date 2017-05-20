2c430b96b59178147ccc2a9a3a09ac715b95ca3c
Said Kaldybaev <said.kaldybaev@gmail.com>
2017-05-21 00:31:04 +0300

    improve cli help

--- lib/football_cli/cli.rb
+++ lib/football_cli/cli.rb
@@ -3,22 +3,22 @@ require_relative 'handler'
 
 module FootballCli
   class CLI < Thor
-    desc 'show', 'sample description'
+    desc 'show', 'Display all the football data feeds'
 
-    option :league
-    option :match_day, type: :numeric
-    option :format
-    option :file
+    option :league, aliases: '-l', banner: 'PD', desc: 'Current standing of league'
+    option :match_day, type: :numeric, aliases: '-d', banner: 5, desc: 'Current standing of league for a given day'
+    option :format, aliases: '-t', banner: 'json', desc: 'Output format type, default: table'
+    option :file, aliases: '-f', banner: 'players.json', desc: 'File name to save the output'
+    option :team, aliases: '-t', banner: 'FCB', desc: 'Team details, players or fixtures'
 
-    option :team
-    class_option :players, type: :boolean
-    class_option :fixtures, type: :boolean
+    class_option :players, type: :boolean, aliases: '-p'
+    class_option :fixtures, type: :boolean, aliases: '-f'
 
     def show
       handler.run
     end
 
-    desc 'live', 'display live scores'
+    desc 'live', 'Display live scores'
     def live
       handler.live_scores
     end
